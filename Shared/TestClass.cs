c4e22026ad8495e769c3414a42174db620b0139d
MRAsoft <info.mrasoft@gmail.com>
2022-07-30 20:10:12 +0200

    Add project files.

---
+++ Shared/TestClass.cs
@@ -0,0 +1,10 @@
+﻿namespace MudBlazorMRA.Shared
+{
+    public class TestClass
+    {
+        public int? ID { get; set; } = 0;
+
+        public string? Description { get; set; } = string.Empty;
+
+    }
+}
